79d69778af868cfa6259ea695ab947784ed3c284
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-11 21:28:42 -0500

    Migración de la tabla catagorias

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 	$ python manage.py runserver 192.168.1.9:8080
+	$ python manage.py makemigrations libros
+	$ python manage.py migrate
--- libros/models.py
+++ libros/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+	class Meta:
+		db_table = 'categorias'
+	nombre = models.CharField(max_length=25)
