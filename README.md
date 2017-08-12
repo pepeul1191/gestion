193bf517ace36e47e17ec8ff24908216bff1d2c0
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-11 22:15:59 -0500

    Archivo de dependencias python añadido

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+	$ pip install -r requirements.txt
 	$ python manage.py runserver 192.168.1.9:8080
 	$ python manage.py makemigrations libros
 	$ python manage.py migrate
--- libros/views.py
+++ libros/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.serializers import serialize
+from .models import Categoria
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	r = serialize('json', Categoria.objects.all())
+	return HttpResponse(r)
