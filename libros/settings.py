e25faf827cd0f1bde98ec158c146a5f106e59af1
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-11 20:56:55 -0500

    Middlewara añadido para setear un header comùn a todo HTTP RR

--- libros/settings.py
+++ libros/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'libros.middleware.MyMiddleware',
 ]
 
 ROOT_URLCONF = 'libros.urls'
