41711b2cad12668df300a96fcf3c738e73e63a8c
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-14 17:04:41 -0500

    Comenzando a hacer los cambios para gestión de libros

--- main/database.py
+++ main/database.py
@@ -5,4 +5,3 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 engine_libros = create_engine('sqlite:///db/db_libros.db')
 engine_accesos = create_engine('sqlite:///db/db_accesos.db')
-engine_ubicaciones = create_engine('sqlite:///db/db_ubicaciones.db')
--- ubicaciones/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	url(r'^distrito/buscar$', views.vw_distrito_provincia_departamento_buscar, name='vw_distrito_provincia_departamento_buscar'),
-	url(r'^departamento/listar$', views.departamento_listar, name='departamento_listar'),
-	url(r'^provincia/listar/(?P<departamento_id>[0-9]+)$', views.provincia_listar, name='provincia_listar'),
-	url(r'^distrito/listar/(?P<provincia_id>[0-9]+)$', views.distrito_listar, name='distrito_listar'),
-]
