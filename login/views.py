d1ef77ad74731b7aa9e35eb0069a71c8b61f42c0
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-15 09:34:00 -0500

    Envío de formulario de login con csrf

--- login/views.py
+++ login/views.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import json
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from main.helper import Helper
 
 def index(request):
-	data = {"mensaje" : False}
-	context = {'helper' : Helper(), 'data':  json.dumps(data),'menu' : '', 'items' : ''}
-	return render(request, 'login/index.html', context)
+    if request.method == 'GET':
+        data = {"mensaje" : False}
+        context = {'helper' : Helper(), 'data':  json.dumps(data),'menu' : '', 'items' : ''}
+        return render(request, 'login/index.html', context)
+    else:
+        return redirect(Helper().get("BASE_URL") + "error/404")
+
+def acceder(request):
+    if request.method == 'POST':
+        data = {"mensaje" : True}
+        context = {'helper' : Helper(), 'data':  json.dumps(data),'menu' : '', 'items' : ''}
+        return render(request, 'login/index.html', context)
+    else:
+        return redirect(Helper().get("BASE_URL") + "error/404")
