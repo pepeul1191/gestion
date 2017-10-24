3efe98af9ea38999634f91f991321d24170aeebe
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-24 16:27:43 -0500

    CRUD de subtitulos

--- accesos/subtitulo.py
+++ accesos/subtitulo.py
@@ -1,13 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import json
+import json, traceback
 from main.helper import Helper
-from main.database import engine_accesos
+from main.database import engine_accesos, session_accesos
 from django.http import HttpResponse
 from sqlalchemy.sql import select
 from .models import Subtitulo
 
 def listar(request, modulo_id):
-	conn = engine_accesos.connect()
-	stmt = select([Subtitulo]).where(Subtitulo.modulo_id == modulo_id)
-	return HttpResponse(json.dumps([dict(r) for r in conn.execute(stmt)]))
+	if request.method == 'GET':
+		conn = engine_accesos.connect()
+		stmt = select([Subtitulo]).where(Subtitulo.modulo_id == modulo_id)
+		return HttpResponse(json.dumps([dict(r) for r in conn.execute(stmt)]))
+
+def guardar(request):
+    if request.method == 'POST':
+        data = json.loads(request.POST.get('data'))
+        nuevos = data['nuevos']
+        editados = data['editados']
+        eliminados = data['eliminados']
+        modulo_id = data['extra']['id_modulo']
+        array_nuevos = []
+        rpta = None
+        session = session_accesos()
+
+        try:
+            if len(nuevos) != 0:
+                for nuevo in nuevos:
+                    temp_id = nuevo['id']
+                    nombre = nuevo['nombre']
+                    s = Subtitulo(nombre = nombre, modulo_id = modulo_id)
+                    session.add(s)
+                    session.flush()
+                    temp = {'temporal' : temp_id, 'nuevo_id' : s.id}
+                    array_nuevos.append(temp)
+            if len(editados) != 0:
+                for editado in editados:
+                    id = editado['id']
+                    nombre = editado['nombre']
+                    session.query(Subtitulo).filter_by(id = id).update(editado)
+            if len(eliminados) != 0:
+                for id in eliminados:
+                    session.query(Subtitulo).filter_by(id = id).delete()
+            session.commit()
+            rpta = {'tipo_mensaje' : 'success', 'mensaje' : ['Se ha registrado los cambios en los subtitulos', array_nuevos]}
+        except Exception as e:
+            session.rollback()
+            rpta = {'tipo_mensaje' : 'error', 'mensaje' : ['Se ha producido un error en guardar la tabla de subtitulos', traceback.format_exc()]}
+
+        return HttpResponse(json.dumps(rpta))
--- accesos/urls.py
+++ accesos/urls.py
@@ -4,16 +4,23 @@ from . import sistema, modulo, subtitulo, item, permiso, rol, usuario, acceso
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	#sistema
+	# sistema
 	url(r'^sistema/listar/$', sistema.listar, name='sistema_listar'),
 	url(r'^sistema/guardar$', sistema.guardar, name='sistema_guardar'),
-	#modulo
+	# modulo
 	url(r'^modulo/listar/(?P<sistema_id>[0-9]+)/$', modulo.listar, name='modulo_listar'),
+	url(r'^modulo/guardar$', modulo.guardar, name='modulo_guardar'),
+	# subtitulo
 	url(r'^subtitulo/listar/(?P<modulo_id>[0-9]+)/$', subtitulo.listar, name='subtitulo_listar'),
+	url(r'^subtitulo/guardar/$', subtitulo.guardar, name='subtitulo_guardar'),
+	# item
 	url(r'^item/listar/(?P<subtitulo_id>[0-9]+)/$', item.listar, name='item_listar'),
+	# rol
 	url(r'^rol/listar/(?P<sistema_id>[0-9]+)/$', rol.listar, name='rol_listar'),
+	# permiso
 	url(r'^permiso/listar/(?P<sistema_id>[0-9]+)/$', permiso.listar, name='permiso_listar'),
 	url(r'^permiso/listar_asociados/(?P<sistema_id>[0-9]+)/(?P<rol_id>[0-9]+)/$', permiso.listar_asociados, name='rol_permisos_asociados'),
+	# usuario
 	url(r'^usuario/listar/$', usuario.listar, name='item_listar'),
 	url(r'^usuario/logs/(?P<usuario_id>[0-9]+)/$', acceso.listar, name='accesos_usuario_listar'),
 	url(r'^usuario/obtener_usuario_correo/(?P<usuario_id>[0-9]+)/$', usuario.obtener_usuario_correo, name='obtener_usuario_correo'),
